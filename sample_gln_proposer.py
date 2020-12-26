b148720263024194456673708117b89bf6fdcc23
nos <siriuself1103@gmail.com>
2020-12-26 15:59:30 -0500

    Version 0.5. Transformer proposer done and debugged

--- sample_gln_proposer.py
+++ sample_gln_proposer.py
@@ -14,12 +14,15 @@ def parse_args():
 
 
 def gln_proposer_test(args):
-    proposer = GLNProposer(model_name=args.model_name,
-                           model_args=None,
-                           model_config={},
-                           data_name="",
-                           processed_data_path=args.processed_data_path,
-                           model_path=args.model_path)
+    proposer = GLNProposer(
+        model_name=args.model_name,
+        model_args=None,
+        model_config={},
+        data_name="",
+        processed_data_path=args.processed_data_path,
+        model_path=args.model_path
+    )
+
     product_smiles = [
         "[Br:1][CH2:2]/[CH:3]=[CH:4]/[C:5](=[O:6])[O:7][Si:8]([CH3:9])([CH3:10])[CH3:11]",
         "[Br:1][c:12]1[cH:11][cH:10][c:9]([OH:15])[c:8]([Cl:7])[c:13]1[Cl:14]"
