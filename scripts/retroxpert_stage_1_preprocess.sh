75997481fcd0a47c6069da55250e084562d8e2e2
nos <siriuself1103@gmail.com>
2021-01-21 13:16:15 -0500

    update preprocess script

---
+++ scripts/retroxpert_stage_1_preprocess.sh
@@ -0,0 +1,9 @@
+python preprocess.py \
+  --model_name="retroxpert" \
+  --data_name="retroxpert_uspto50k" \
+  --log_file="retroxpert_uspto50k_preprocess" \
+  --train_file="./data/retroxpert_uspto50k/raw/train.csv" \
+  --val_file="./data/retroxpert_uspto50k/raw/valid.csv" \
+  --test_file="./data/retroxpert_uspto50k/raw/test.csv" \
+  --processed_data_path="./data/retroxpert_uspto50k/processed/" \
+  --num_cores=8
