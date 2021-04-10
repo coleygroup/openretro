45c999c5de37b8ee42afec13d843e7b7fef6177d
nos <ztu@mit.edu>
2021-04-10 10:13:10 -0400

    intermediate version for gln_uspto_full; got some efficiency issues

--- scripts/gln_preprocess.sh
+++ scripts/gln_preprocess.sh
@@ -1,9 +1,15 @@
+#!/bin/bash
+
+# Loading the required module
+module load anaconda/2020b
+source activate openretro
+
 python preprocess.py \
   --model_name="gln" \
-  --data_name="schneider50k" \
-  --log_file="gln_preprocess" \
-  --train_file="./data/gln_schneider50k/raw/raw_train.csv" \
-  --val_file="./data/gln_schneider50k/raw/raw_val.csv" \
-  --test_file="./data/gln_schneider50k/raw/raw_test.csv" \
-  --processed_data_path="./data/gln_schneider50k/processed" \
-  --num_cores=8
+  --data_name="uspto_full" \
+  --log_file="gln_preprocess_uspto_full" \
+  --train_file="./data/gln_uspto_full/raw/raw_train.csv" \
+  --val_file="./data/gln_uspto_full/raw/raw_val.csv" \
+  --test_file="./data/gln_uspto_full/raw/raw_test.csv" \
+  --processed_data_path="./data/gln_uspto_full/processed" \
+  --num_cores=20
