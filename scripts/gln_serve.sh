4a422c7b7ed47e4839b933a9ab360d5c4521a155
nos <ztu@mit.edu>
2021-05-31 17:35:39 -0400

    added in 50k raw files (since they are not that big)

--- scripts/gln_serve.sh
+++
@@ -1,5 +0,0 @@
-torchserve \
-  --start \
-  --ncs \
-  --model-store=./checkpoints/gln_schneider50k/model-6.dump \
-  --models gln_50k_untyped=gln_50k_untyped.mar
--- scripts/retroxpert_serve.sh
+++
@@ -1,5 +0,0 @@
-torchserve \
-  --start \
-  --ncs \
-  --model-store=./checkpoints/retroxpert_uspto50k_untyped \
-  --models retroxpert_uspto50k_untyped=retroxpert_uspto50k_untyped.mar
