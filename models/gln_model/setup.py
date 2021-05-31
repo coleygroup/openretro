19e9ccb6f0066867d62f0342a5f51581d3eb7d83
nos <ztu@mit.edu>
2021-05-31 18:15:38 -0400

    gln setup fix for environ

--- models/gln_model/setup.py
+++ models/gln_model/setup.py
@@ -25,8 +25,8 @@ ext_modules=[CppExtension('extlib',
 
 # build cuda lib
 import torch
-gpu_flag = getattr(os.environ, "GPU_FLAG", 0)
-if torch.cuda.is_available() or int(gpu_flag) == 1:
+gpu_flag = os.environ.get("GPU_FLAG")
+if torch.cuda.is_available() or (gpu_flag is not None and int(gpu_flag) == 1):
     print("building gln with CUDA support")
     ext_modules.append(CUDAExtension('extlib_cuda',
                                     ['gln/mods/torchext/src/extlib_cuda.cpp', 'gln/mods/torchext/src/extlib_cuda_kernels.cu']))
