52ba9e9911ec32ad0c78ef97eb4dab58f54dd3c8
nos <ztu@mit.edu>
2021-06-01 15:51:48 -0400

    hide setup.sh, train in docker to be debugged

--- scripts/gln_train.sh
+++
@@ -1,8 +0,0 @@
-python train.py \
-  --do_train \
-  --model_name="gln" \
-  --data_name="schneider50k" \
-  --log_file="gln_train" \
-  --train_file="./data/gln_schneider50k/raw/raw_train.csv" \
-  --processed_data_path="./data/gln_schneider50k/processed" \
-  --model_path="./checkpoints/gln_schneider50k"
