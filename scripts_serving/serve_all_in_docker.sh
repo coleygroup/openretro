2b6cad2e2d25f7fced4f8531f3733d7e1c0df864
nos <ztu@mit.edu>
2022-04-22 13:46:10 -0400

    scripts update; retroxpert serving passed

--- scripts_serving/serve_all_in_docker.sh
+++
@@ -1,7 +0,0 @@
-docker run -p 8080:8080 -p 8081:8081 -p 8082:8082 \
-  -t openretro:serving-cpu \
-  --models \
-  gln_50k_untyped=gln_50k_untyped.mar \
-  neuralsym_50k=neuralsym_50k.mar \
-  retroxpert_uspto50k_untyped=retroxpert_uspto50k_untyped.mar \
-  transformer_50k_untyped=transformer_50k_untyped.mar
