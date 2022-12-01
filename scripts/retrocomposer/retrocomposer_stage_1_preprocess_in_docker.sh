e75ec85ec29dbddf39e9caa571e01cd31485527e
Zhengkai Tu <ztu@mit.edu>
2022-12-01 14:00:19 -0500

    scripts and Dockerfile update for RetroComposer (adding indigo)

---
+++ scripts/retrocomposer/retrocomposer_stage_1_preprocess_in_docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+docker run \
+  -v "$PWD/logs":/app/openretro/logs \
+  -v "$PWD/checkpoints":/app/openretro/checkpoints \
+  -v "$PWD/results":/app/openretro/results \
+  -v "$TRAIN_FILE":/app/openretro/data/tmp_for_docker/raw_train.csv \
+  -v "$VAL_FILE":/app/openretro/data/tmp_for_docker/raw_val.csv \
+  -v "$TEST_FILE":/app/openretro/data/tmp_for_docker/raw_test.csv \
+  -v "$PROCESSED_DATA_PATH_RETROCOMPOSER":/app/openretro/data/tmp_for_docker/processed \
+  -t openretro:gpu \
+  python preprocess.py \
+  --model_name="retrocomposer" \
+  --stage=1 \
+  --data_name="$DATA_NAME" \
+  --log_file="retrocomposer_preprocess_s1_$DATA_NAME" \
+  --train_file=/app/openretro/data/tmp_for_docker/raw_train.csv \
+  --val_file=/app/openretro/data/tmp_for_docker/raw_val.csv \
+  --test_file=/app/openretro/data/tmp_for_docker/raw_test.csv \
+  --processed_data_path=/app/openretro/data/tmp_for_docker/processed \
+  --prod_k=1 \
+  --react_k=1 \
+  --num_cores="$NUM_CORES"
